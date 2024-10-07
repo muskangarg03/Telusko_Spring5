5c6b5dfca7432f3366bad5520cacf00eedd69d5b
muskangarg03 <muskangarg676@gmail.com>
2024-10-07 13:27:55 +0530

    Ref attribute

---
+++ 03-SpringCore(IOC)/06-RefAttribute/src/main/java/com/telusko/SpringDemo/Alien.java
@@ -0,0 +1,32 @@
+package com.telusko.SpringDemo;
+
+public class Alien 
+{
+	
+	private int age;
+	private Laptop laptop;
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		System.out.println("Age Assigned");
+		this.age = age;
+		
+	}
+
+	public Alien() {
+		System.out.println("Alien Object Created..");
+	}
+	
+	public void code() {
+		System.out.println("I am Coding..");
+		laptop.compile();
+	}
+
+}
