4f38f2778ab49f1c6cd6cf5f719d44abd2f2e5ab
muskangarg03 <muskangarg676@gmail.com>
2024-10-07 12:47:39 +0530

    Singleton and Prototype

---
+++ 03-SpringCore(IOC)/04-SingletonAndPrototype/src/main/java/com/telusko/SpringDemo/App.java
@@ -0,0 +1,32 @@
+package com.telusko.SpringDemo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");    //configuration file should be present at the location "src/main/java"
+    	
+//    	Alien obj1 = (Alien)factory.getBean("alien"); 	//getBean() method returns a object type, so we need to type-caste it
+//        obj1.code();
+//        obj1.age = 15;
+//        System.out.println(obj1.age);
+//        
+//        Alien obj2 = (Alien)factory.getBean("alien"); 	//getBean() method returns a object type, so we need to type-caste it
+//        obj2.code();
+//
+//        System.out.println(obj2.age);
+        
+        //All Spring beans are Singleton beans by default
+        //By default, Spring follows Singleton Design Pattern, it gives one object
+    	
+        
+    }
+}
