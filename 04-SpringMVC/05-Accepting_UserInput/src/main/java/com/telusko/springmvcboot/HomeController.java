5e2e53b92a6575b186edee3f16a98012f71f1598
muskangarg03 <muskangarg676@gmail.com>
2024-10-08 16:30:49 +0530

    user input

---
+++ 04-SpringMVC/05-Accepting_UserInput/src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -0,0 +1,33 @@
+package com.telusko.springmvcboot;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController 
+{
+	@RequestMapping("/")
+	public String home()
+	{
+		//System.out.println("home page requested");
+		return "index.jsp";     //It is called by DispatcherServlet
+	}
+	
+	@RequestMapping("add")
+	public String add(HttpServletRequest req)
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int num3 = i+j;
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("num3", num3);
+		
+		return "result.jsp";
+	}
+	
+}
