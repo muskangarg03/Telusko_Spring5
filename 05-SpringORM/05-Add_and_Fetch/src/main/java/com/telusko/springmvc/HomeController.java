1013f1994617fab5c360ca7e71d7d39a6b8bc2ca
muskangarg03 <muskangarg676@gmail.com>
2024-10-10 15:57:17 +0530

    add fetch

---
+++ 05-SpringORM/05-Add_and_Fetch/src/main/java/com/telusko/springmvc/HomeController.java
@@ -0,0 +1,68 @@
+package com.telusko.springmvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.telusko.springmvc.dao.AlienDao;
+import com.telusko.springmvc.dao.AlienDao;
+import com.telusko.springmvc.model.Alien;
+
+@Controller
+public class HomeController 
+{
+	@Autowired
+	private AlienDao dao;
+	
+	@ModelAttribute       //@ModelAttribute is called first before calling any RequestMapping
+	public void modelDaata(Model m)
+	{
+		m.addAttribute("name", "Aliens");
+	}
+	
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		//System.out.println("home page requested");
+		return "index";     //It is called by DispatcherServlet
+	}
+	
+		
+//	@Transactional
+	@GetMapping("getAliens")
+	public String getAlien(Model m) 
+	{
+		m.addAttribute("result",dao.getAliens());
+		return "showAliens";
+	}
+
+	
+	@GetMapping("getAlien")
+	public String getAlien(@RequestParam int aid, Model m) 
+	{
+		m.addAttribute("result",dao.getAlien(aid));
+		return "showAliens";
+	}
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@ModelAttribute("result") Alien a) 
+	{
+		dao.addAlien(a);
+		return "showAliens";
+	}
+	
+	
+}
