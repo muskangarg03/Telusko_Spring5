009f9417c6a62f7851fa941c920d479545a18546
muskangarg03 <muskangarg676@gmail.com>
2024-10-14 17:30:36 +0530

    add and fetch

---
+++ 06-SpringDataJPA/03-JPARepository_Add_and_Fetch/src/main/java/com/telusko/springmvcboot/DemoTeluskoApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.springmvcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTeluskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTeluskoApplication.class, args);
+	}
+
+}
