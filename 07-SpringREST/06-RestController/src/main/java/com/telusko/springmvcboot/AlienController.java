32d56112f969f32d71f0dede8eee16c708a89b7c
muskangarg03 <muskangarg676@gmail.com>
2024-10-17 12:33:29 +0530

    rest controller

---
+++ 07-SpringREST/06-RestController/src/main/java/com/telusko/springmvcboot/AlienController.java
@@ -0,0 +1,43 @@
+package com.telusko.springmvcboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.telusko.springmvcboot.model.Alien;
+
+
+@RestController      //it is used for both @Controller and @ResponseBody     
+public class AlienController {
+
+	@Autowired
+	AlienRepo repo;
+	
+	
+
+	//@ResponseBody
+	@GetMapping("aliens")
+	public List<Alien> getAlien() 
+	{
+		List<Alien> aliens  = repo.findAll();		
+		
+		return aliens;	//it will return the data in json format
+	}
+	
+	
+	
+	//@ResponseBody
+	@GetMapping("alien/{aid}")
+	public Alien getAlien(@PathVariable("aid") int aid) {
+		
+		Alien alien = repo.findById(aid).orElse(new Alien(0,""));
+		
+		return alien;
+	}
+}
