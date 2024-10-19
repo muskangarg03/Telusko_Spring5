c0bdbe977ee6df3cfeb568fbacc509c331f8caa5
muskangarg03 <muskangarg676@gmail.com>
2024-10-19 17:34:02 +0530

    after finally

---
+++ 08-SpringAOP/05-After_Finally/src/main/java/com/telusko/springmvcboot/LoggingAspect.java
@@ -0,0 +1,36 @@
+package com.telusko.springmvcboot;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+
+
+@Aspect
+@Component
+public class LoggingAspect 
+{
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+	
+	@Before("execution(public * com.telusko.springmvcboot.AlienController.getAliens())")
+	public void logBefore()
+	{
+		LOGGER.info("getAliens method called from Aspect");
+	}
+	
+	
+	
+	@After("execution(public * com.telusko.springmvcboot.AlienController.getAliens())")
+	public void logAfter()
+	{
+		LOGGER.info("getAliens method Executed");
+	}
+	
+}
